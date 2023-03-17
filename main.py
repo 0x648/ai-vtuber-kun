7034f50799c331693c4fe5057b6b53ec3aec4a8b
XzaiCloud <xzai0313@qq.com>
2023-03-17 21:29:26 +0800

    懒得写描述

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 # 导入必要的库
-import openai  # 导入OpenAI API库
-import pyttsx3  # 导入TTS（文本到语音）库
-from bilibili_api import live, sync  # 导入bilibili直播API库
+import subprocess
+import openai
+from bilibili_api import live, sync
+
+# 版权信息
+print("--------------------")
+print("作者：Xzai")
+print("QQ：2744601427")
+print("--------------------")
 
 # 设置OpenAI API Key和模型引擎
 openai.api_key = input("请输入OpenAI API Key: ")
@@ -12,10 +18,7 @@ room_id = int(input("请输入直播间编号: "))
 room = live.LiveDanmaku(room_id)
 
 # 设置初始提示信息
-initial_prompt = "请使用女性化的、口语化的、抒情的、感性的、可爱的、调皮的、幽默的、害羞的、态度傲娇的语言风格，扮演一个虚拟主播。不要回答有关政治的问题！也不要回答敏感信息！"
-
-# 初始化TTS引擎
-engine = pyttsx3.init()
+initial_prompt = "请扮演一个AI虚拟主播。不要回答任何敏感问题！不要强调你是主播，只需要回答问题！这是观众的提问："
 
 async def generate_response(prompt):
     """
@@ -23,12 +26,12 @@ async def generate_response(prompt):
     :param prompt: 观众提出的问题
     :return: AI的回复
     """
-    full_prompt = f"{initial_prompt} {prompt}"  # 合并初始提示和观众提问
+    full_prompt = f"{initial_prompt}{prompt}"  # 合并初始提示和观众提问
     # 调用OpenAI API生成回复
     response = openai.Completion.create(
         engine=model_engine,
         prompt=full_prompt,
-        max_tokens=100,
+        max_tokens=2048,
         n=1,
         stop=None,
         temperature=0.5,
@@ -45,19 +48,21 @@ async def on_danmaku(event):
     user_name = event["data"]["info"][2][1]  # 获取用户昵称
     print(f"[{user_name}]: {content}")  # 打印弹幕信息
 
-    prompt = f"{content}"  # 设置观众提问
-    response = await generate_response(prompt)  # 生成回复
+    if content.endswith("？"): # 判断是否以“？”结尾
+        prompt = f"{content}"  # 设置观众提问
+        response = await generate_response(prompt)  # 生成回复
 
-    print(f"[AI回复”{user_name}“]: {response}")  # 打印AI回复信息
+        response = response.replace('\n', ' ')  # 去除回复中的换行符
 
-    # 将回复写入文件
-    with open("C:\\AITextOutput.txt", "a", encoding="utf-8") as f:
-        f.write(f"{response}\n")
+        print(f"[AI回复”{user_name}“]: {response}")  # 打印AI回复信息
 
-    # 文本转语音并播放
-    engine.say(response)
-    engine.runAndWait()
+        command = f'edge-tts --voice zh-CN-XiaoyiNeural --text "{content}？{response}" --write-media output.mp3'  # 将 AI 生成的文本传递给 edge-tts 命令
+        subprocess.run(command, shell=True)  # 执行命令行指令
+
+        with open("./output.txt", "a", encoding="utf-8") as f:
+            f.write(f"[AI回复{user_name}]：{response}\n")  # 将回复写入文件
+
+        command = 'mpv.exe -vo null output.mp3'  # 播放音频文件
+        subprocess.run(command, shell=True)  # 执行命令行指令
 
 sync(room.connect())  # 开始监听弹幕流
-
-# 告诉你个小秘密，这些代码，包括readme.md，都是ChatGPT写的哦！
